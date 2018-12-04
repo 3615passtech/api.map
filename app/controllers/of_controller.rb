ca53917cb6233c5815a1da6dbfd071e5859658b5
Nicolas Hovart <kola.hov@gmail.com>
2018-12-04 13:57:58 +0100

    Add a bootcamp in Json

--- app/controllers/of_controller.rb
+++ app/controllers/of_controller.rb
@@ -2,7 +2,8 @@ class OfController < ApplicationController
    
     def index
         @data = [
-            {"lat":0.2, "long":32.5, "name":"Le premier bootcamp"}
+            {"lat":0.2, "long":32.5, "name":"Le premier bootcamp"},
+            {"lat":11.7, "long":30.5, "name":"Le deuxième bootcamp"}
         ]
         render json: @data
     end
